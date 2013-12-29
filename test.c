3e03205d1291be7eb8d5bcc91ad31dcb8a27917d
Isaac Turner <turner.isaac@gmail.com>
2013-12-29 15:18:12 +0000

    Tidied up test to take cmdline args; check result

--- test.c
+++ test.c
@@ -1,70 +1,79 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/time.h> // for seeding random
 #include <unistd.h> // getpid
+#include <stdarg.h>
 #include "mpmc.h"
 
 // Queue
 MPMCQueue q;
-const int qlen = 1000;
+size_t qlen = 1000;
 
 // 100 producers, 100 consumers, each producer produces 10000
-const int nproducers = 10, nconsumers = 10, proc_count = 100000;
+size_t nproducers = 10, nconsumers = 10, proc_count = 1000000;
 
-pthread_t prod_threads[nproducers], cons_threads[nconsumers];
-int prod_ids[nproducers], cons_ids[nconsumers];
-
-void seed_random()
+void print_usage()
 {
-  struct timeval time;
-  gettimeofday(&time, NULL);
-  srand((((time.tv_sec ^ getpid()) * 1000001) + time.tv_usec));
+  printf("usage: test [options]\n"
+"    -p <p>  Number of producer threads [default: %zu]\n"
+"    -c <c>  Number of consumer threads [default: %zu]\n"
+"    -n <n>  Number of messages per producer [default: %zu]\n"
+"    -q <q>  Queue length [default: %zu]\n",
+         nproducers, nconsumers, proc_count, qlen);
+  exit(EXIT_FAILURE);
 }
 
-void* producer(void *ptr)
+struct TestThread {
+  pthread_t th;
+  size_t id, result;
+};
+
+struct TestThread *producers, *consumers;
+
+void* produce(void *ptr)
 {
-  int w, id = *(int*)ptr, start = id * proc_count, end = start + proc_count;
-  printf("Created producer %i\n", id);
-  for(w = start; w < end; w++) {
-    mpmc_write(&q, &w);
-    // printf("prod %i: %i\n", id, w);
-    // sleep(1);
-  }
-  printf("Producer %i finished!\n", id);
+  struct TestThread *prod = (struct TestThread*)ptr;
+  size_t w, id = prod->id, start = id * proc_count, end = start + proc_count;
+  printf("Created producer %zu\n", id);
+  for(w = start; w < end; w++) mpmc_write(&q, &w);
+  printf("Producer %zu finished!\n", id);
   pthread_exit(NULL);
 }
 
-void* consumer(void *ptr)
+void* consume(void *ptr)
 {
-  int id = *(int*)ptr, r;
-  printf("Created consumer %i\n", id);
+  struct TestThread *cons = (struct TestThread*)ptr;
+  size_t id = cons->id, r, sum = 0;
+  printf("Created consumer %zu\n", id);
   while(mpmc_read(&q, &r)) {
-    // printf("cons%i: %i\n", id, r);
-    printf("%i\n", r);
-    // sleep(1);
+    // printf("%zu\n", r);
+    sum += r;
   }
-  printf("Consumer %i finished!\n", id);
+  printf("Consumer %zu finished!\n", id);
+  cons->result = sum;
   pthread_exit(NULL);
 }
 
 void init_threads()
 {
-  int i, rc;
+  size_t i; int rc;
   pthread_attr_t thread_attr;
   pthread_attr_init(&thread_attr);
   pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
 
+  producers = malloc(nproducers * sizeof(struct TestThread));
+  consumers = malloc(nconsumers * sizeof(struct TestThread));
+
   // create consumers
   for(i = 0; i < nconsumers; i++) {
-    cons_ids[i] = i;
-    rc = pthread_create(&cons_threads[i], &thread_attr, consumer, &cons_ids[i]);
+    consumers[i].id = i;
+    rc = pthread_create(&consumers[i].th, &thread_attr, consume, &consumers[i]);
     if(rc != 0) { fprintf(stderr, "Creating thread failed\n"); exit(-1); }
   }
 
   // create producers
   for(i = 0; i < nproducers; i++) {
-    prod_ids[i] = i;
-    rc = pthread_create(&prod_threads[i], &thread_attr, producer, &prod_ids[i]);
+    producers[i].id = i;
+    rc = pthread_create(&producers[i].th, &thread_attr, produce, &producers[i]);
     if(rc != 0) { fprintf(stderr, "Creating thread failed\n"); exit(-1); }
   }
 
@@ -72,7 +81,7 @@ void init_threads()
 
   // Wait for producers to finish
   for(i = 0; i < nproducers; i++) {
-    rc = pthread_join(prod_threads[i], NULL);
+    rc = pthread_join(producers[i].th, NULL);
     if(rc != 0) { fprintf(stderr, "Join thread failed\n"); exit(-1); }
   }
 
@@ -82,22 +91,45 @@ void init_threads()
   for(i = 0; i < 100; i++)
     mpmc_write(&q, &i);
 
+  size_t extra_sum = 100*(99/2.0);
+
   mpmc_close(&q);
   printf("waiting for consumers to finish...\n");
+  size_t sum = 0, nmsg = proc_count * nproducers;
 
   // Wait until finished
   for(i = 0; i < nconsumers; i++) {
-    rc = pthread_join(cons_threads[i], NULL);
+    rc = pthread_join(consumers[i].th, NULL);
     if(rc != 0) { fprintf(stderr, "Join thread failed\n"); exit(-1); }
+    sum += consumers[i].result;
   }
 
+  size_t corr_sum = nmsg*((nmsg-1)/2.0) + extra_sum;
+  printf("messages: %zu result: %zu [%s %zu]\n",
+         nmsg, sum, sum == corr_sum ? "PASS" : "FAIL", corr_sum);
+
   pthread_attr_destroy(&thread_attr);
+  free(producers);
+  free(consumers);
 }
 
-int main()
+int main(int argc, char **argv)
 {
+  // Read args
+  int c;
+
+  while ((c = getopt(argc, argv, "p:c:n:q:h")) >= 0)
+    if (c == 'p') nproducers = atoi(optarg);
+    else if (c == 'c') nconsumers = atoi(optarg);
+    else if (c == 'n') proc_count = atoi(optarg);
+    else if (c == 'q') qlen = atoi(optarg);
+    else print_usage();
+
   // Create queue of ints of length qlen
-  mpmc_alloc(&q, qlen, sizeof(int), nproducers, nconsumers);
+  mpmc_alloc(&q, qlen, sizeof(size_t), nproducers, nconsumers);
+
+  printf("nproducers: %zu nconsumers: %zu n: %zu qlen: %zu\n",
+         nproducers, nconsumers, proc_count, qlen);
 
   init_threads();
 
